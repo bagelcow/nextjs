7c6cd26ac1892815b602c95b460295d35a8ad179
bagelcow <102549803+bagelcow@users.noreply.github.com>
2022-05-10 22:38:49 +0100

    2022

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-      <a>𝗟𝗘𝗞𝗦.𝗚𝗚</a> © {new Date().getFullYear()}
+      <span><a>𝗟𝗘𝗞𝗦.𝗚𝗚</a> © {new Date().getFullYear()}</span>
       </footer>
     </div>
   )
